0fd5097665f223eaf6aa4fbc2742c2a94d98ddb4
Chad Chaney <cchane4@gmail.com>
2019-10-18 23:12:17 -0500

    displaying movie data from the api via  a template

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ let express = require("express");
 let app = express(); 
 let request = require("request"); 
 
+app.set("view engine", "ejs"); 
+
+
 app.get("/results",(req, res) => { 
     request("http://www.omdbapi.com/?apikey=a4db347d&s=illinois",(error, response, body) => { 
         if(!error && response.statusCode == 200){ 
-            let results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]); 
+            let data = JSON.parse(body);
+            res.render("results", {data: data}); 
         }
     }); 
 });
