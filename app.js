93c32a71f6bf39d10786d3c4abc3df8b63d56062
Chad Chaney <cchane4@gmail.com>
2019-10-19 06:28:02 -0500

    adding a search feature

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ let request = require("request");
 
 app.set("view engine", "ejs"); 
 
+app.get("/", (req, res) => { 
+    res.render("search"); 
+})
 
 app.get("/results",(req, res) => { 
-    request("http://www.omdbapi.com/?apikey=a4db347d&s=illinois",(error, response, body) => { 
+    let query = req.query.search;
+    let url = "http://www.omdbapi.com/?apikey=a4db347d&s=" + query;
+    request(url,(error, response, body) => { 
         if(!error && response.statusCode == 200){ 
             let data = JSON.parse(body);
             res.render("results", {data: data}); 
